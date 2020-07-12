5660f83f915508b8ec1f72a4af1229103fca908f
Moeez Saiyam <moeez.saiyam@rolustech.net>
2020-07-12 23:42:38 +0500

    Set Admin credientials

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#set admin user and password
+ADMIN_EMAIL="moeezsaiyam@gmail.com"
+ADMIN_PASSWORD="test@123"
