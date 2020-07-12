fb7e5ea190de880add4bbb514d27863622c7c31d
Moeez Saiyam <moeez.saiyam@rolustech.net>
2020-07-12 23:53:28 +0500

    set env variables v1

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-#set admin user and password
-ADMIN_EMAIL="moeezsaiyam@gmail.com"
-ADMIN_PASSWORD="test@123"
